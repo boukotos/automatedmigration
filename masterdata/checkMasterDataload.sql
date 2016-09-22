722ceffbe7f096a5b44c5bfc5ce58b2d4886fc2e
boukotos <rachid.boukotos@pearlchain.net>
2016-09-22 11:42:29 +0200

    PRL-10259 : load template, scheme, meterclass, meter and related BOM via queue migration

--- masterdata/checkMasterDataload.sql
+++ masterdata/checkMasterDataload.sql
@@ -7,12 +7,12 @@ BEGIN
 counter := 1 ;
 WHILE counter != 0
 LOOP
-   SELECT count(0) INTO counter FROM if_message WHERE QUEUENAME in ('ProductIn','SingleProductIn') AND STATUS = 'NEW';
+   SELECT count(0) INTO counter FROM if_message WHERE QUEUENAME in ('ProductIn','migration') AND STATUS = 'NEW';
    counter := counter;
    dbms_lock.sleep( 2 ); 
 END LOOP;
 
-SELECT count(0) INTO counter FROM if_message WHERE QUEUENAME ='ProductIn' AND STATUS = 'ERROR';
+SELECT count(0) INTO counter FROM if_message WHERE QUEUENAME in ('ProductIn','migration') AND STATUS = 'ERROR';
 IF counter!=0 THEN 
 	RAISE migration_error;
 END IF;
