c4f58130dc4e9b65b628cb307d8dc243c43b1d5f
boukotos <rachid.boukotos@pearlchain.net>
2016-09-27 13:50:40 +0200

    improvements

--- contract/loadcontract.cmd
+++ contract/loadcontract.cmd
@@ -1,17 +1,28 @@
 rem export NLS_LANG=AMERICAN_AMERICA.WE8ISO8859P1
-set ORACLE_SID=XE
-
-sqlldr canonmig/pearlchain ContractHeader.ctl
-sqlldr canonmig/pearlchain ContractLine.ctl
-sqlldr canonmig/pearlchain ContractInvoiceSpecification.ctl
-sqlldr canonmig/pearlchain ContractObject.ctl
-sqlldr canonmig/pearlchain ContractTimingTerm.ctl
-sqlldr canonmig/pearlchain ContractClusterSpecification.ctl
-sqlldr canonmig/pearlchain ContractCondition.ctl
-sqlldr canonmig/pearlchain ContractLeaseCalculation.ctl
-sqlldr canonmig/pearlchain ContractRevenueAllocation.ctl
-
-sqlplus canonmig/pearlchain @cdact.sql
-sqlplus canonmig/pearlchain @gxaction.sql
+set ORACLE_SID=ORCL
+sqlldr c##staging2/canon ContractHeader.ctl
+if ERRORLEVEL 1 GOTO fout
+sqlldr c##staging2/canon ContractLine.ctl
+if ERRORLEVEL 1 GOTO fout
+sqlldr c##staging2/canon ContractInvoiceSpecification.ctl
+if ERRORLEVEL 1 GOTO fout
+sqlldr c##staging2/canon ContractObject.ctl
+if ERRORLEVEL 1 GOTO fout
+sqlldr c##staging2/canon ContractTimingTerm.ctl
+if ERRORLEVEL 1 GOTO fout
+sqlldr c##staging2/canon ContractClusterSpecification.ctl
+if ERRORLEVEL 1 GOTO fout
+sqlldr c##staging2/canon ContractCondition.ctl
+if ERRORLEVEL 1 GOTO fout
+sqlldr c##staging2/canon ContractLeaseCalculation.ctl
+if ERRORLEVEL 1 GOTO fout
+sqlldr c##staging2/canon ContractRevenueAllocation.ctl
+if ERRORLEVEL 1 GOTO fout
+sqlplus c##staging2/canon @checkload.sql
+IF NOT ERRORLEVEL 1 GOTO :EOF
+echo  error : checkload contract>> automigration.log
+exit /b 1
+:fout
+exit /b 1
 
 pause
