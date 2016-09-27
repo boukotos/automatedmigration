bec3f9a80d751adfe95a2fefd1a03894c41209f7
boukotos <rachid.boukotos@pearlchain.net>
2016-09-27 13:51:52 +0200

    improvements

--- contract/checkAfterload.sql
+++ contract/checkAfterload.sql
@@ -4,7 +4,7 @@ DECLARE
    PRAGMA EXCEPTION_INIT(migration_error, -20000);
    counter NUMBER;
 BEGIN
-SELECT count(0) INTO counter FROM GX WHERE GXSTAT ='FINISHED';
+SELECT count(0) INTO counter FROM GX WHERE GXSTAT ='FAILED';
 IF counter!=0 THEN 
 	RAISE migration_error;
 END IF;
